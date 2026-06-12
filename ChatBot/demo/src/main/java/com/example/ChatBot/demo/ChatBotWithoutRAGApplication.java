e3629a4e809cb80a3ad9aceaaa3804a6005c674f
Vennila Natrajan <57398585+vennila19@users.noreply.github.com>
2026-06-12 06:08:11 +0000

    Latest changes

--- ChatBot/demo/src/main/java/com/example/ChatBot/demo/ChatBotWithoutRAGApplication.java
+++ ChatBot/demo/src/main/java/com/example/ChatBot/demo/ChatBotWithoutRAGApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ChatBotWithoutRAGApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ChatBotWithoutRAGApplication.class, args);
 	}
 
 }
